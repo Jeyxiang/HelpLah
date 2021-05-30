c2810634c55ca347b34fabeb8966e8cc6b2fa1cb
Haozeee <teyhaoze@gmail.com>
2021-05-30 23:11:39 +0800

    Business registration added

---
+++ app/src/main/java/com/example/helplah/models/JobRequests.java
@@ -0,0 +1,2 @@
+package com.example.helplah.models;public class JobRequests {
+}
---
+++ app/src/main/java/com/example/helplah/viewmodel/login_screen/SecBizRegActivity.java
@@ -0,0 +1,14 @@
+package com.example.helplah.viewmodel.login_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecBizRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec_biz_reg);
+    }
+}
