5f1d50579066416d7d365d7de5958f024878ae47
Haozeee <teyhaoze@gmail.com>
2021-06-02 22:28:39 +0800

    checkbox fixed

--- app/src/main/java/com/example/helplah/adapters/CheckboxAdapter.java
+++ app/src/main/java/com/example/helplah/adapters/CheckboxAdapter.java
@@ -16,55 +16,55 @@ import java.util.List;
 
 public class CheckboxAdapter extends RecyclerView.Adapter<CheckboxAdapter.MyViewHolder> {
 
-        String services[];
-        ArrayList<String> arrayList;
-        Context context;
+    String services[];
+    ArrayList<String> arrayList;
+    Context context;
 
-        public CheckboxAdapter(Context ct, String s1[]) {
-            services = s1;
-            context = ct;
-        }
+    public CheckboxAdapter(Context ct, String s1[]) {
+        this.services = s1;
+        this.context = ct;
+        this.arrayList = new ArrayList<>();
+    }
 
-        @NonNull
-        @Override
-        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(context);
-            View view = inflater.inflate(R.layout.checkbox_reg,parent,false);
-            return new CheckboxAdapter.MyViewHolder(view);
-        }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.checkbox_reg,parent,false);
+        return new CheckboxAdapter.MyViewHolder(view);
+    }
 
-        @Override
-        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-            holder.chckboxes.setText(services[position]);
-            holder.chckboxes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (holder.chckboxes.isChecked()) {
-                        arrayList.add(services[position]);
-                    } else {
-                        arrayList.remove(services[position]);
-                    }
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.chckboxes.setText(services[position]);
+        holder.chckboxes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.chckboxes.isChecked()) {
+                    arrayList.add(services[position]);
+                } else {
+                    arrayList.remove(services[position]);
                 }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return services.length;
-        }
-
-        public class MyViewHolder extends RecyclerView.ViewHolder {
-            CheckBox chckboxes;
-
-            public MyViewHolder(@NonNull View itemView) {
-                super(itemView);
-                chckboxes = itemView.findViewById(R.id.checkBox);
-                arrayList = new ArrayList<String>();
             }
-        }
+        });
+    }
 
-        public List<String> listofSelected() {
-            return arrayList;
+    @Override
+    public int getItemCount() {
+        return services.length;
+    }
+
+    public List<String> listofSelected() {
+    return arrayList;
+}
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        CheckBox chckboxes;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            chckboxes = itemView.findViewById(R.id.checkBox);
         }
     }
+}
 
